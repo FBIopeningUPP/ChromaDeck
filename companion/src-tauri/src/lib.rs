c518dc6a743c6abf244211d3961b98cea7a973b5
Milind Singh <milindsingh486@gmail.com>
2026-08-10 16:26:53 +0530

    hve some errors

--- companion/src-tauri/src/lib.rs
+++ companion/src-tauri/src/lib.rs
@@ -1,14 +1,23 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use midir::MidiInput;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn list_midi_ports() -> Vec<String> {
+    let mut available_ports = Vec::new();
+    if let Ok(midi_in) = MidiInput::new("ChromaDeck Companion Reader") {
+        for port in midi_in.ports().iter() {
+            if let Ok(port_name) = midi_in.port_name(port) {
+                available_ports.push(port_name);
+            }
+        }
+    }
+    available_ports
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
+        .invoke_handler(tauri::generate_handler![list_midi_ports])
+        .run(tauri::generate_handler!())
         .expect("error while running tauri application");
 }
